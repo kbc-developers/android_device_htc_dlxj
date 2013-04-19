ebc8ebfee1247a64d423e72014fdbd292406a142
Flyhalf205 <Dhays90@gmail.com>
2013-04-19 16:31:45 +0900

    Adding Enhanced NFC

--- cm.mk
+++ cm.mk
@@ -11,6 +11,9 @@ PRODUCT_RELEASE_NAME := dlxj
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/htc/dlxj/device_dlxj.mk)
 
