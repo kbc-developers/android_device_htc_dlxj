25511c86900b3d18189ac329229d92e5ab5195c2
tako0910 <tako.oct0910@gmail.com>
2013-05-19 12:04:32 +0900

    Add ChameleonOS setting file

---
+++ cos.mk
@@ -0,0 +1,29 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cos/config/gsm.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Release name
+PRODUCT_RELEASE_NAME := dlxj
+
+# Inherit some common COS stuff.
+$(call inherit-product, vendor/cos/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cos/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/dlxj/device_dlxj.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := dlxj
+PRODUCT_NAME := cos_dlxj
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := HTC J butterfly
+PRODUCT_MANUFACTURER := HTC
+
+# Set build fingerprint / ID / Product Name etc.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_dlxj BUILD_FINGERPRINT=verizon_wwe/dlxj/dlxj:4.1.1/JRO03C/123160.4:user/release-keys PRIVATE_BUILD_DESC="1.15.605.4 CL123160 release-keys" BUILD_NUMBER=123160
+
