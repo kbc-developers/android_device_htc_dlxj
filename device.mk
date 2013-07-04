a1ee7fa3ffdc2c0365fc701131675613779d8ed2
tako0910 <tako.oct0910@gmail.com>
2013-07-05 00:29:58 +0900

    fix more more typo

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 ## (2) Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/htc/dlx/dlxj-vendor.mk)
+$(call inherit-product-if-exists, vendor/htc/dlxj/dlxj-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/htc/dlxj/overlay
