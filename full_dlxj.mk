591cdc76a9999d499c14aa05d645055593a89a1c
David Hays <dhays90@gmail.com>
2013-08-01 22:13:01 -0500

    dlx: Remove vold.fstab
    Change-Id: Ibd3f3a51e9bbdffa14f6aacb5605f79059c06776

--- full_dlxj.mk
+++ full_dlxj.mk
@@ -67,10 +67,6 @@ PRODUCT_COPY_FILES += device/htc/dlxj/configs/AudioBTID.csv:system/etc/AudioBTID
 PRODUCT_COPY_FILES += device/htc/dlxj/configs/AudioBTIDnew.csv:system/etc/AudioBTIDnew.csv
 PRODUCT_COPY_FILES += device/htc/dlxj/configs/audio_effects.conf:system/etc/audio_effects.conf
 
-# vold config
-PRODUCT_COPY_FILES += \
-    device/htc/dlxj/configs/vold.fstab:system/etc/vold.fstab
-
 # wifi config
 PRODUCT_COPY_FILES += \
     device/htc/dlxj/configs/calibration:/system/etc/calibration \
