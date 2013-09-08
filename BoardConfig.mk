TeamWin/android_device_htc_dlx

dlx: use m7 kernel repo
Change-Id: I6c1dd71f0f101b24a709400dc0888ed8d6062e3a

BoardConfig.mk
@@ -52,10 +52,10 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 #Kernel
 BOARD_KERNEL_BASE := 0x80600000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=dlx user_debug=0  
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=dlx user_debug=31
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
 TARGET_KERNEL_CONFIG := cyanogenmod_dlx_defconfig
-TARGET_KERNEL_SOURCE := kernel/htc/dlx
+TARGET_KERNEL_SOURCE := kernel/htc/m7
 
 # Audio
 BOARD_USES_FLUENCE_INCALL := true

kbc-developers/android_device_htc_dlxj

Conflicts:
	BoardConfig.mk
	cm.dependencies

BoardConfig.mk
@@ -52,10 +52,10 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 #Kernel
 BOARD_KERNEL_BASE := 0x80600000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=dlxj user_debug=0 
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=dlxj user_debug=31
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
 TARGET_KERNEL_CONFIG := cyanogenmod_deluxe_j_defconfig
-TARGET_KERNEL_SOURCE := kernel/htc/dlxj
+TARGET_KERNEL_SOURCE := kernel/htc/m7wlj
 
 # Audio
 BOARD_USES_FLUENCE_INCALL := true