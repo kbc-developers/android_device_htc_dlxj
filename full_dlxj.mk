5e0fa344bb7205fc71a783b8c43de79b7b732b9d
Christopher R. Palmer <crpalmer@gmail.com>
2013-09-12 19:43:26 -0400

    Add the M7 media_profiles.xml to stop FFC force closes
    Change-Id: Id6c62968233ef6ea1b6d868091c0e84764d0f8e4

--- full_dlxj.mk
+++ full_dlxj.mk
@@ -90,6 +90,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/htc/dlxj/dsp/snd_soc_msm/snd_soc_msm_2x_Fusion3:/system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3
 
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+
 # Keylayouts and Keychars
 PRODUCT_COPY_FILES += \
     device/htc/dlxj/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
