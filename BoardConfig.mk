2ea72e11de0180477f2e794cee4b16411d217d26
Dan Pasanen <dan.pasanen@gmail.com>
2013-10-21 21:46:26 -0500

    dlx: use mainline libril
    Change-Id: I15badd8add091e7f5debfff2bea03b3a63721b7b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,10 +73,6 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/dlxj/bluetooth/include
 BOARD_BLUEDROID_VENDOR_CONF := device/htc/dlxj/bluetooth/vnd_dlxj.txt
 BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
 
-# Use libril in the device tree
-BOARD_PROVIDES_LIBRIL := true
-COMMON_GLOBAL_CFLAGS += -DNEW_LIBRIL_HTC
-
 # USB
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
