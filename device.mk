TeamWin/android_device_htc_dlx

Revert "dlx: don't build without proprietary blobs"
This reverts commit 0033219217abc04de895852e732db023513b0571.

Change-Id: I4fb2ff3139da0fb690edd4e3acb6c13127cb4a5c

device.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 #
 
-## Get non-open-source specific aspects
-$(call inherit-product, vendor/htc/dlx/dlx-vendor.mk)
+## (2) Also get non-open-source specific aspects if available
+$(call inherit-product-if-exists, vendor/htc/dlx/dlx-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/htc/dlx/overlay

kbc-developers/android_device_htc_dlxj

Conflicts:
	device.mk

device.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 #
 
-## Get non-open-source specific aspects
-$(call inherit-product, vendor/htc/dlxj/dlxj-vendor.mk)
+## (2) Also get non-open-source specific aspects if available
+$(call inherit-product-if-exists, vendor/htc/dlxj/dlxj-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/htc/dlxj/overlay