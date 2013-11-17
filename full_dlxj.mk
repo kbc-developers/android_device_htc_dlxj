07d50367aa9395456d25929f9565330cf5f8d333
blunden <blunden2@gmail.com>
2013-11-17 21:42:03 +0100

    Enable NFC host-based card emulation
    Change-Id: I18db06bf0788aed7d5169d1c839d96d4df34741d

--- full_dlxj.mk
+++ full_dlxj.mk
@@ -58,6 +58,7 @@ PRODUCT_COPY_FILES += \
     $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
 
 
