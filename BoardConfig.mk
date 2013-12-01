c7d6a8c65bd7c5390eef87a40044d60c5e516193
Dan Pasanen <dan.pasanen@gmail.com>
2013-12-01 18:50:13 +0000

    Merge "update for new adreno blobs (overrides msm8960-common)" into cm-11.0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,6 +40,9 @@ TARGET_OTA_ASSERT_DEVICE := dlxj
 TARGET_BOOTLOADER_BOARD_NAME := dlxj
 BOARD_HAS_LARGE_FILESYSTEM := true
 
+# Adreno configuration
+BOARD_EGL_CFG := device/htc/dlx/configs/egl.cfg
+
 #Kernel
 BOARD_KERNEL_BASE := 0x80600000
 BOARD_KERNEL_PAGESIZE := 2048
