wrapped/du_android_device_htc_dlx

dlx: Kang extract-files.sh / setup-makefiles.sh
.. from mondrianwifi from hlte to allow me to pull the blobs from
a local copy instead of the device.

Change-Id: Ib6223c358c76b0ebaccc8ffe2d5505e79a11710d

extract-files.sh
@@ -1,17 +1,56 @@
-#!/bin/sh
+#!/bin/bash
 
-VENDOR=htc
-DEVICE=dlx
+set -e
+export DEVICE=dlx
+export VENDOR=htc
+
+if [ $# -eq 0 ]; then
+  SRC=adb
+else
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
+fi
 
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
 
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$ | sed -e 's#^/system/##g'`; do
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
+for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
+  echo "Extracting /system/$FILE ..."
+  OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+  FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+  DEST=${PARSING_ARRAY[1]}
+  if [ -z $DEST ]
+  then
+    DEST=$FILE
+  fi
+  DIR=`dirname $FILE`
+  if [ ! -d $BASE/$DIR ]; then
+    mkdir -p $BASE/$DIR
+  fi
+  if [ "$SRC" = "adb" ]; then
+    adb pull /system/$FILE $BASE/$DEST
+  # if file dot not exist try destination
+    if [ "$?" != "0" ]
+        then
+        adb pull /system/$DEST $BASE/$DEST
     fi
-    adb pull /system/$FILE $BASE/$FILE
+  else
+    cp $SRC/system/$FILE $BASE/$DEST
+    # if file dot not exist try destination
+    if [ "$?" != "0" ]
+        then
+        cp $SRC/system/$DEST $BASE/$DEST
+    fi
+  fi
 done
 
 ./setup-makefiles.sh

setup-makefiles.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
-VENDOR=htc
-DEVICE=dlx
+#!/bin/bash
+
+export VENDOR=htc
+export DEVICE=dlx
+
 OUTDIR=vendor/$VENDOR/$DEVICE
 MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
 
@@ -11,7 +13,7 @@ MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,12 +28,23 @@ EOF
 
 LINEEND=" \\"
 COUNT=`wc -l proprietary-files.txt | awk {'print $1'}`
-for FILE in `cat proprietary-files.txt`; do
-    COUNT=`expr $COUNT - 1`
-    if [ $COUNT = "0" ]; then
-        LINEEND=""
+DISM=`egrep -c '(^#|^$)' proprietary-files.txt`
+COUNT=`expr $COUNT - $DISM`
+for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
+  COUNT=`expr $COUNT - 1`
+  if [ $COUNT = "0" ]; then
+    LINEEND=""
+  fi
+      # Split the file from the destination (format is "file[:destination]")
+  OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+  if [[ ! "$FILE" =~ ^-.* ]]; then
+    FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+    DEST=${PARSING_ARRAY[1]}
+    if [ -n "$DEST" ]; then
+      FILE=$DEST
     fi
-    echo "    $OUTDIR/proprietary/$FILE:system/$FILE$LINEEND" >> $MAKEFILE
+    echo "	$OUTDIR/proprietary/$FILE:system/$FILE$LINEEND" >> $MAKEFILE
+  fi
 done
 
 (cat << EOF) > ../../../$OUTDIR/$DEVICE-vendor.mk
@@ -41,7 +54,7 @@ done
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -50,9 +63,6 @@ done
 # limitations under the License.
 
 # This file is generated by device/$VENDOR/$DEVICE/setup-makefiles.sh
-
-# Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/$VENDOR/$DEVICE/overlay
 
 \$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)
 EOF
@@ -64,7 +74,7 @@ EOF
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -73,7 +83,4 @@ EOF
 # limitations under the License.
 
 # This file is generated by device/$VENDOR/$DEVICE/setup-makefiles.sh
-
 EOF
-
-

kbc-developers/android_device_htc_dlxj

Conflicts:
	extract-files.sh
	setup-makefiles.sh

extract-files.sh
@@ -1,17 +1,56 @@
-#!/bin/sh
+#!/bin/bash
 
-VENDOR=htc
-DEVICE=dlxj
+set -e
+export DEVICE=dlxj
+export VENDOR=htc
+
+if [ $# -eq 0 ]; then
+  SRC=adb
+else
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
+fi
 
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
 
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$ | sed -e 's#^/system/##g'`; do
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
+for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
+  echo "Extracting /system/$FILE ..."
+  OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+  FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+  DEST=${PARSING_ARRAY[1]}
+  if [ -z $DEST ]
+  then
+    DEST=$FILE
+  fi
+  DIR=`dirname $FILE`
+  if [ ! -d $BASE/$DIR ]; then
+    mkdir -p $BASE/$DIR
+  fi
+  if [ "$SRC" = "adb" ]; then
+    adb pull /system/$FILE $BASE/$DEST
+  # if file dot not exist try destination
+    if [ "$?" != "0" ]
+        then
+        adb pull /system/$DEST $BASE/$DEST
     fi
-    adb pull /system/$FILE $BASE/$FILE
+  else
+    cp $SRC/system/$FILE $BASE/$DEST
+    # if file dot not exist try destination
+    if [ "$?" != "0" ]
+        then
+        cp $SRC/system/$DEST $BASE/$DEST
+    fi
+  fi
 done
 
 ./setup-makefiles.sh

setup-makefiles.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
-VENDOR=htc
-DEVICE=dlxj
+#!/bin/bash
+
+export VENDOR=htc
+export DEVICE=dlxj
+
 OUTDIR=vendor/$VENDOR/$DEVICE
 MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
 
@@ -11,7 +13,7 @@ MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,12 +28,23 @@ EOF
 
 LINEEND=" \\"
 COUNT=`wc -l proprietary-files.txt | awk {'print $1'}`
-for FILE in `cat proprietary-files.txt`; do
-    COUNT=`expr $COUNT - 1`
-    if [ $COUNT = "0" ]; then
-        LINEEND=""
+DISM=`egrep -c '(^#|^$)' proprietary-files.txt`
+COUNT=`expr $COUNT - $DISM`
+for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
+  COUNT=`expr $COUNT - 1`
+  if [ $COUNT = "0" ]; then
+    LINEEND=""
+  fi
+      # Split the file from the destination (format is "file[:destination]")
+  OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+  if [[ ! "$FILE" =~ ^-.* ]]; then
+    FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+    DEST=${PARSING_ARRAY[1]}
+    if [ -n "$DEST" ]; then
+      FILE=$DEST
     fi
-    echo "    $OUTDIR/proprietary/$FILE:system/$FILE$LINEEND" >> $MAKEFILE
+    echo "	$OUTDIR/proprietary/$FILE:system/$FILE$LINEEND" >> $MAKEFILE
+  fi
 done
 
 (cat << EOF) > ../../../$OUTDIR/$DEVICE-vendor.mk
@@ -41,7 +54,7 @@ done
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -50,9 +63,6 @@ done
 # limitations under the License.
 
 # This file is generated by device/$VENDOR/$DEVICE/setup-makefiles.sh
-
-# Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/$VENDOR/$DEVICE/overlay
 
 \$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)
 EOF
@@ -64,7 +74,7 @@ EOF
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -73,7 +83,6 @@ EOF
 # limitations under the License.
 
 # This file is generated by device/$VENDOR/$DEVICE/setup-makefiles.sh
-
 EOF
 
 (cat << EOF) > ../../../$OUTDIR/Android.mk