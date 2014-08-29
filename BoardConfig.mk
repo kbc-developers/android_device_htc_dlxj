031d2ada5088241cbfcfb67400e7d091e6bfae38
Dan Pasanen <dan.pasanen@gmail.com>
2014-08-29 09:29:27 -0500

    fstab: get in the now
    * we use one fstab for rom and recovery these days
    
    Change-Id: I4633edabc5a0386aa35b4662213ba20470f3801d
    
    Conflicts:
    
    	rootdir/etc/fstab.dlxj

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,3 +116,4 @@ BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_RECOVERY_SWIPE := true
+TARGET_RECOVERY_FSTAB := device/htc/dlxj/rootdir/etc/fstab.dlxj
