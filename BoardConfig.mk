cfdd3f36439ba2429870e468058ce3933052fa9d
Dan Pasanen <dan.pasanen@gmail.com>
2014-12-15 21:46:39 -0600

    dlx: offmode charging for lollipop
    * Rid ourselves of lpm.rc and instead use the on charger trigger
    * Add selinux policies to go along with this
    
    Change-Id: Ibfb4b6e8df3df7ce9f93953229ebbbe5874496b5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,7 +97,9 @@ BOARD_SEPOLICY_UNION += \
     mm-qcamerad.te \
     mpdecision.te \
     netmgrd.te \
+    property_contexts \
     qmuxd.te \
+    recovery.te \
     surfaceflinger.te \
     system_server.te \
     tee.te \
