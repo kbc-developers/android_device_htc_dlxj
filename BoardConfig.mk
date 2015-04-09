70f405986a089191b1bb91be8ee574ab95198761
Dan Pasanen <dan.pasanen@gmail.com>
2015-04-09 17:21:20 -0500

    cmhw: add DisplayColorCalibration for PCC
    * also add appropriate selinux rules and permissions
    
    Change-Id: Icf94f9584c56d38246b6c11ed94065a25f6aeec9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,6 +67,9 @@ BOARD_NEEDS_MEMORYHEAPPMEM := true
 COMMON_GLOBAL_CFLAGS += -DHTC_CAMERA_HARDWARE
 TARGET_RELEASE_CPPFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 
+# CMHW
+BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw
+
 # Graphics
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 HAVE_ADRENO_SOURCE := false
@@ -91,6 +94,7 @@ BOARD_SEPOLICY_UNION += \
     drmserver.te \
     felica.te \
     file_contexts \
+    file.te \
     hcheck.te \
     healthd.te \
     init.te \
