e9336c07346a08e685eed9719138d3ec571065ea
Thierry Strudel <tstrudel@google.com>
2015-07-16 18:30:40 -0700

    dlx: force dex2oat not to use swap file
    Change-Id: Ieaebd24834e42b23b84b2f30f306472c01054770

--- full_dlxj.mk
+++ full_dlxj.mk
@@ -109,6 +109,10 @@ PRODUCT_COPY_FILES += \
     device/htc/dlxj/idc/projector_input.idc:system/usr/idc/projector_input.idc \
     device/htc/dlxj/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc
 
+# Art
+PRODUCT_PROPERTY_OVERRIDES += \
+	dalvik.vm.dex2oat-swap=false
+
 # Audio
 PRODUCT_PACKAGES += \
 	audio_amplifier.msm8960
