wrapped/du_android_device_htc_dlx

dlx: Add a fancy custom ril to set mQANElements
This fixes the search or network operators.

Change-Id: I02be33be2da18cbaa45146ca81a70ddde891246d
JIRA: CYAN-6253

BoardConfig.mk
@@ -79,8 +79,9 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/dlx/bluetooth/include
 BOARD_BLUEDROID_VENDOR_CONF := device/htc/dlx/bluetooth/vnd_dlx.txt
 BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
 
-# Use libril in the device tree
+# Radio
 BOARD_PROVIDES_LIBRIL := true
+BOARD_RIL_CLASS := ../../../device/htc/dlx/ril
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk

kbc-developers/android_device_htc_dlxj

BoardConfig.mk
@@ -79,9 +79,9 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/dlxj/bluetooth/include
 BOARD_BLUEDROID_VENDOR_CONF := device/htc/dlxj/bluetooth/vnd_dlxj.txt
 BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
 
-# Use libril in the device tree
+# Radio
 BOARD_PROVIDES_LIBRIL := true
-COMMON_GLOBAL_CFLAGS += -DNEW_LIBRIL_HTC
+BOARD_RIL_CLASS := ../../../device/htc/dlxj/ril
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk