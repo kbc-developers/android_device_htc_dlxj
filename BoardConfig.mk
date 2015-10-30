4f60300f25b94b67bc12c84600d0c3ffb52498ad
Dan Pasanen <dan.pasanen@gmail.com>
2015-10-30 08:14:28 -0500

    cmdline: remove no-op
    Change-Id: I150bd172ea6c75b0454dd7faa47a8700ca0293d1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,7 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 #Kernel
 BOARD_KERNEL_BASE := 0x80600000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=dlxj androidboot.selinux=permissive user_debug=0 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=dlxj androidboot.selinux=permissive user_debug=0
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
 TARGET_KERNEL_CONFIG := cyanogenmod_deluxe_j_defconfig
 TARGET_KERNEL_SOURCE := kernel/htc/m7wlj
