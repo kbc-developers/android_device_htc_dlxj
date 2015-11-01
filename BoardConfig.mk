0a9ab88ded5e07109c9d635ab94ec2d7827436ac
Steve Kondik <steve@cyngn.com>
2015-11-01 12:58:40 -0800

    dlx: Switch to Clang by default
    Change-Id: I97016bf134aea6d59dc8307f6543c1d2f3bbf53a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,7 @@ TARGET_SPECIFIC_HEADER_PATH += device/htc/dlxj/include
 # Flags
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+USE_CLANG_PLATFORM_BUILD := true
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := dlxj
