30c46898d19c758db390258f040d1d2337529687
Christopher R. Palmer <crpalmer@gmail.com>
2015-11-17 09:05:56 -0500

    dlx: Remove nfce_access_debug.xml
    Change-Id: Ifa9cac5ebbdf32ac3761acc844861c5eeaf993d3

--- full_dlxj.mk
+++ full_dlxj.mk
@@ -45,16 +45,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libnetcmdiface
 
-# NFCEE access control
-ifeq ($(TARGET_BUILD_VARIANT),user)
-    NFCEE_ACCESS_PATH := device/htc/dlxj/configs/nfcee_access.xml
-else
-    NFCEE_ACCESS_PATH := device/htc/dlxj/configs/nfcee_access_debug.xml
-endif
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
-    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml \
+    device/htc/dlxj/configs/nfcee_access.xml:system/etc/nfcee_access.xml \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
